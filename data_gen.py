09e287bcf3af4e41e48dd7ef4e2f28a22b0a5dbd
Andrew <arharris@mun.ca>
2024-11-04 15:59:43 -0330

    Basic variable relation

--- data_gen.py
+++ data_gen.py
@@ -7,6 +7,27 @@ class NormalVariable:
         self.__std = std
         self.__rng = np.random.default_rng()
     
+    def set_mean(self, new_mean: float):
+        self.__mean = new_mean
+    
     def get(self, n: int = 1) -> float | np.ndarray[float]:
         result = np.zeros(n) + self.__mean + (self.__rng.normal(size=n) * self.__std)
         return result[0] if n == 1 else result
+
+
+class VariableRelation:
+
+    def __init__(self, inputs: list[tuple[NormalVariable, float]], output: NormalVariable):
+        if type(inputs[0]) is NormalVariable:
+            raise TypeError("Weights must be included in `inputs`!")
+
+        self.__inputs = inputs
+        self.__output = output
+    
+    def get(self, n: int = 1) -> np.ndarray[float]:
+        result = np.zeros((len(self.__inputs) + 1, n))
+        for i, (input, weight) in enumerate(self.__inputs):
+            result[i] = input.get(n)
+            result[-1] += result[i] * weight
+
+        return result
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,11 +1,12 @@
-from data_gen import NormalVariable
-from pytest import fixture, approx
+from data_gen import NormalVariable, VariableRelation
+from pytest import fixture, approx, raises
 import numpy as np
 
 
 @fixture
 def static_var():
     return NormalVariable(5, 0)
+
 
 @fixture
 def unit_var():
@@ -24,3 +25,18 @@ def test_generate_std(unit_var):
     result = unit_var.get(1000)
     assert not np.array_equal(result, np.zeros(1000))
     assert result.mean() == approx(0, abs=0.1)
+
+
+def test_error_on_no_weights(static_var, unit_var):
+    with raises(TypeError):
+        rel = VariableRelation([static_var], unit_var)
+
+
+def test_direct_relation():
+    var1 = NormalVariable(3, 0)
+    var2 = NormalVariable(0, 0)
+
+    rel = VariableRelation([(var1, 2)], var2)
+    assert np.array_equal(rel.get(), [[3], [6]])
+    assert np.array_equal(rel.get(5), [[3, 3, 3, 3, 3], [6, 6, 6, 6, 6]])
+
