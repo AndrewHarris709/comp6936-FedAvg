c6ee5d6e7e7eb50b108f26a98f0f4ab6e8e861f6
Ali Bahari <abahari@terrence2.cs.mun.ca>
2024-11-06 12:06:45 -0330

    Add multi-feature linear regression

--- soloLinearRegression.py
+++ soloLinearRegression.py
@@ -8,8 +8,6 @@ trainTestSplit = 0.1
 
 data_X, data_Y = datasets.load_diabetes(return_X_y = True)
 
-data_X = data_X[:, np.newaxis, 3]
-
 numTest = int(data_X.shape[0] * trainTestSplit)
 X_train = data_X[:-numTest]; X_test = data_X[-numTest:]
 Y_train = data_Y[:-numTest]; Y_test = data_Y[-numTest:]
@@ -21,10 +19,3 @@ Y_pred = regressor.predict(X_test)
 print(f"Coefficients (weights): {regressor.coef_}")
 print(f"Independent Term (bias): {regressor.intercept_}")
 print(f"MSE: {mean_squared_error(Y_test, Y_pred)}")
-
-plt.scatter(X_test, Y_test)
-plt.plot(X_test, Y_pred, color = "black")
-# plt.xticks(())
-# plt.yticks(())
-
-plt.show()
