c292bbf4890acfe7c004dd0c04df5c8b94289b05
Andrew <arharris@mun.ca>
2024-11-06 16:08:58 -0330

    Refactor noise in data generation

--- data_gen.py
+++ data_gen.py
@@ -1,31 +1,51 @@
 import numpy as np
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
-class NormalVariable:
+@dataclass
+class GeneratorVariable(ABC):
+    lower_base: float
+    upper_base: float
+    weight: float = 1
+    rng: np.random.Generator = np.random.default_rng()
 
-    def __init__(self, mean: float, std: float):
-        self.__mean = mean
-        self.__std = std
-        self.__rng = np.random.default_rng()
-    
-    def set_mean(self, new_mean: float):
-        self.__mean = new_mean
-    
-    def get_base(self) -> float:
-        return self.__mean
-    
-    def get(self, n: int = 1, means: np.ndarray[float] = None) -> float | np.ndarray[float]:
-        if means is None:
-            result = np.zeros(n) + self.__mean
-        else:
-            result = np.zeros(n) + means
+    def get_bases(self, n: int = 1) -> np.ndarray[float]:
+        return self.rng.uniform(self.lower_base, self.upper_base, n)
 
-        result += (self.__rng.normal(size=n) * self.__std)
-        return result[0] if n == 1 else result
+    @abstractmethod
+    def generate(self, bases: np.ndarray[float]) -> np.ndarray[float]:
+        pass
+    
+    #def set_mean(self, new_mean: float):
+        #self.__mean = new_mean
+    
+    #def get_base(self) -> float:
+        #return self.__mean
+    
+    #def get(self, n: int = 1, means: np.ndarray[float] = None) -> float | np.ndarray[float]:
+    #    if means is None:
+    #        result = np.zeros(n) + self.__mean
+    #    else:
+    #        result = np.zeros(n) + means
+
+    #    result += (self.__rng.normal(size=n) * self.__std)
+    #    return result[0] if n == 1 else result
+
+
+@dataclass
+class NormalVariable(GeneratorVariable):
+    std: float = 0
+    noise: float = 0
+
+    def generate(self, bases: np.ndarray[float]) -> np.ndarray[float]:
+        result = self.rng.normal(loc=bases, size=len(bases), scale=self.std)
+        result += self.rng.uniform(low=-self.noise, high=self.noise, size=len(bases))
+        return result
 
 
 class VariableRelation:
 
-    def __init__(self, inputs: list[tuple[NormalVariable, float]], output: NormalVariable):
+    def __init__(self, inputs: list[tuple[NormalVariable]], output: NormalVariable):
         if type(inputs[0]) is NormalVariable:
             raise TypeError("Weights must be included in `inputs`!")
 
@@ -35,9 +55,10 @@ class VariableRelation:
     def get(self, n: int = 1) -> np.ndarray[float]:
         result = np.zeros((len(self.__inputs) + 1, n))
         for i, (input, weight) in enumerate(self.__inputs):
-            result[i] = input.get(n)
-            result[-1] += input.get_base() * weight
+            bases = input.get_bases()
+            result[i] = input.generate(bases)
+            result[-1] += bases * weight
         
-        result[-1] = self.__output.get(n, result[-1])
+        result[-1] = self.__output.generate(result[-1])
 
         return result
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,30 +1,50 @@
-from data_gen import NormalVariable, VariableRelation
+from data_gen import GeneratorVariable, NormalVariable, VariableRelation
 from pytest import fixture, approx, raises
 import numpy as np
 
 
 @fixture
 def static_var():
-    return NormalVariable(5, 0)
+    return NormalVariable(lower_base=5, upper_base=5, weight=1, std=0, noise=0)
 
 
 @fixture
 def unit_var():
-    return NormalVariable(0, 1)
+    return NormalVariable(lower_base=0, upper_base=0, weight=1, std=1, noise=0)
+
+
+def test_fail_on_not_implemented():
+    with raises(TypeError):
+        rel = GeneratorVariable().generate()
+
+
+def test_get_one_base(static_var):
+    assert static_var.get_bases() == 5
+
+
+def test_get_five_bases(static_var):
+    assert np.array_equal(static_var.get_bases(5), [5, 5, 5, 5, 5])
 
 
 def test_generate_one(static_var):
-    assert static_var.get() == 5
+    assert static_var.generate([5]) == 5
 
 
 def test_generate_five(static_var):
-    assert np.array_equal(static_var.get(5), [5, 5, 5, 5, 5])
+    assert np.array_equal(static_var.generate(np.array([5, 5, 5, 5, 5])), [5, 5, 5, 5, 5])
 
 
 def test_generate_std(unit_var):
-    result = unit_var.get(1000)
+    result = unit_var.generate(unit_var.get_bases(1000))
     assert not np.array_equal(result, np.zeros(1000))
     assert result.mean() == approx(0, abs=0.1)
+
+
+def test_generate_noise():
+    var = NormalVariable(lower_base=0, upper_base=0, weight=1, std=0, noise=1)
+    result = var.generate(var.get_bases(1000))
+    assert not np.array_equal(result, np.zeros(1000))
+    assert np.mean(result) == approx(0, abs=0.1)
 
 
 def test_error_on_no_weights(static_var, unit_var):
@@ -33,8 +53,8 @@ def test_error_on_no_weights(static_var, unit_var):
 
 
 def test_direct_relation():
-    var1 = NormalVariable(3, 0)
-    var2 = NormalVariable(0, 0)
+    var1 = NormalVariable(lower_base=3, upper_base=3, weight=2, std=0, noise=0)
+    var2 = NormalVariable(lower_base=3, upper_base=3, std=0, noise=0)
 
     rel = VariableRelation([(var1, 2)], var2)
     assert np.array_equal(rel.get(), [[3], [6]])
