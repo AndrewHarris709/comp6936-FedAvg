5607fd1ee45cd34b4088449be8074969de7161b4
Ali Bahari <abahari@terrence2.cs.mun.ca>
2024-11-06 14:53:34 -0330

    Add keras-based linear regression

--- soloLinearRegression.py
+++ soloLinearRegression.py
@@ -1,21 +1,71 @@
+import keras
+from sklearn import datasets
+# from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error
 
-trainTestSplit = 0.1
+def get_model(inputDim, outputDim):
+    inputs = keras.Input(shape = (inputDim,))
+    x = keras.layers.Dense(outputDim, activation = "linear")(inputs)
+    outputs = keras.layers.Dense(outputDim)(x)
+
+    model = keras.Model(inputs = inputs, outputs = outputs, name = "LinearRegression")
+    model.compile(
+        loss = keras.losses.MeanSquaredError(),
+        optimizer = keras.optimizers.Adam(),
+        metrics = []
+    )
+    return model
+
+
 
 data_X, data_Y = datasets.load_diabetes(return_X_y = True)
 
-numTest = int(data_X.shape[0] * trainTestSplit)
-X_train = data_X[:-numTest]; X_test = data_X[-numTest:]
-Y_train = data_Y[:-numTest]; Y_test = data_Y[-numTest:]
+data_X = data_X[:, np.newaxis, 3] # Choosing only one feature
 
-regressor = linear_model.LinearRegression()
-regressor.fit(X_train, Y_train)
-Y_pred = regressor.predict(X_test)
+train_X, test_X, train_Y, test_Y = train_test_split(data_X, data_Y, test_size = 0.1, random_state = 1)
 
-print(f"Coefficients (weights): {regressor.coef_}")
-print(f"Independent Term (bias): {regressor.intercept_}")
-print(f"MSE: {mean_squared_error(Y_test, Y_pred)}")
+# scaler = StandardScaler()
+# train_X = scaler.fit(train_X).transform(train_X)
+# test_X = scaler.fit(test_X).transform(test_X)
+
+if(len(train_Y.shape) == 1):
+    outDim = 1
+else:
+    outDim = train_Y.shape[1]
+
+model = get_model(inputDim = train_X.shape[1], outputDim = outDim)
+history = model.fit(
+    train_X,
+    train_Y,
+    batch_size = 1,
+    epochs = 150,
+    validation_data = (test_X, test_Y)
+)
+
+weightDense1 = model.layers[1].get_weights()[0]
+biasDense1 = model.layers[1].get_weights()[1]
+weightDense2 = model.layers[2].get_weights()[0]
+biasDense2 = model.layers[2].get_weights()[1]
+finalWeight = weightDense1 * weightDense2
+finalBias = weightDense2 * biasDense1 + biasDense2
+
+pred_loss = model.evaluate(test_X, test_Y)
+print(f"Prediction Loss: {pred_loss}")
+print(f"Coefficients (weights): {finalWeight}")
+print(f"Independent Term (bias): {finalBias}")
+
+# plt.plot(history.history["loss"])
+# plt.plot(history.history["val_loss"])
+# plt.title(f"Linear Regression MSE")
+# plt.ylabel("Loss")
+# plt.xlabel("Epoch")
+# plt.legend(["Train", "Val"], loc = "upper left")
+# plt.show()
+
+plt.scatter(test_X, test_Y)
+plt.plot(test_X, model.predict(test_X), color = "black")
+
+plt.show()
