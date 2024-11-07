617609ed7d0f29c746f7e7c56228452415948b37
Ali Bahari <abahari@terrence2.cs.mun.ca>
2024-11-07 15:57:56 -0330

    Add model params separation

--- linearRegression/utils.py
+++ linearRegression/utils.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import mean_squared_error
+import numpy as np
 
 def fit_keras_model(model, X, Y, batchSize = 1, epochs = 100):
     return model.fit(
@@ -11,8 +12,18 @@ def fit_keras_model(model, X, Y, batchSize = 1, epochs = 100):
 def get_keras_loss(model, X, Y):
     return model.evaluate(X, Y)
 
+def get_keras_params(model):
+    w1, b1 = model.layers[1].get_weights()
+    w2, b2 = model.layers[2].get_weights()
+    wTotal = np.multiply(w1, w2)
+    bTotal = w2 * b1 + b2
+    return wTotal, bTotal
+
 def fit_sklearn_model(model, X, Y):
     return model.fit(X, Y)
 
 def get_sklearn_loss(pred_Y, target_Y):
     return mean_squared_error(pred_Y, target_Y)
+
+def get_sklearn_params(model):
+    return model.coef_, model.intercept_
--- soloLinearRegression.py
+++ soloLinearRegression.py
@@ -2,7 +2,6 @@ from sklearn import datasets
 import numpy as np
 # import matplotlib.pyplot as plt
 
-
 from linearRegression.models import *
 from linearRegression.utils import *
 
@@ -13,7 +12,6 @@ data_X, data_Y = datasets.load_diabetes(return_X_y = True)
 if(columnIdx >= 0):
     data_X = data_X[:, np.newaxis, columnIdx]
 
-
 if(modelType == "keras"):
     if(len(data_Y.shape) == 1):
         outDim = 1
@@ -22,20 +20,16 @@ if(modelType == "keras"):
     model = get_keras_model(inputDim = data_X.shape[1], outputDim = outDim)
     history = fit_keras_model(model, data_X, data_Y)
     print(f"Keras Loss: {get_keras_loss(model, data_X, data_Y)}")
+    weight, bias = get_keras_params(model)
+    print(f"Keras Weight: {weight}")
+    print(f"Keras Bias: {bias}")
 else:
     model = get_sklearn_model()
     model = fit_sklearn_model(model, data_X, data_Y)
     print(f"Sklearn Loss: {get_sklearn_loss(model.predict(data_X), data_Y)}")
-
-# weightDense1 = model.layers[1].get_weights()[0]
-# biasDense1 = model.layers[1].get_weights()[1]
-# weightDense2 = model.layers[2].get_weights()[0]
-# biasDense2 = model.layers[2].get_weights()[1]
-# finalWeight = weightDense1 * weightDense2
-# finalBias = weightDense2 * biasDense1 + biasDense2
-
-# print(f"Coefficients (weights): {finalWeight}")
-# print(f"Independent Term (bias): {finalBias}")
+    weight, bias = get_sklearn_params(model)
+    print(f"Sklearn Weights: {weight}")
+    print(f"Sklearn Biases: {bias}")
 
 # plt.plot(history.history["loss"])
 # plt.plot(history.history["val_loss"])
