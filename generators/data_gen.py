88e3912fd9c753fcc6b3b453ce88d7f3c15e4c3b
Andrew Harris <arharris@mun.ca>
2024-11-13 23:55:01 -0330

    Demonstration of CholeskyGenerator

--- generators/data_gen.py
+++ generators/data_gen.py
@@ -2,23 +2,33 @@ import numpy as np
 
 class CholeskyGenerator:
 
-    def __init__(self, corr: np.ndarray[float], shifts: np.ndarray[float],
+    def __init__(self, corr: np.ndarray[float], shifts: np.ndarray[float] = None, mults: np.ndarray[float] = None,
                  rng: np.random.Generator = np.random.default_rng()):
         
         if type(corr) is list:
             corr = np.array(corr)
         if type(shifts) is list:
             shifts = np.array(shifts)
+        if type(mults) is list:
+            mults = np.array(mults)
+
+        if mults is None:
+            mults = np.zeros(len(corr))
+        if shifts is None:
+            shifts = np.zeros(len(corr))
 
         if not (corr==corr.T).all():
             raise TypeError("Correlation Matrix is not symmetric!")
 
         self.__L = np.linalg.cholesky(corr)
         self.__shifts = shifts.reshape((len(self.__L), 1))
+        self.__mults = mults.reshape((len(self.__L), 1))
         self.__rng = rng
 
     def get(self, n: int = 1):
         result = self.__rng.normal(loc=0, scale=1, size=(len(self.__L), n)).reshape((len(self.__L), n))
+        result = self.__L @ result
+        result = result * self.__mults
         result = self.__shifts + result
 
         return result
--- tests/test_cholesky.py
+++ tests/test_cholesky.py
@@ -49,15 +49,45 @@ def test_simple_gen(rng_mock):
 
 
 def test_simple_gen_shift(rng_mock):
-    simple_gen = CholeskyGenerator(np.array([[1, 0], [0, 1]]), np.array([1, 1]), rng=rng_mock)
+    simple_gen = CholeskyGenerator(np.array([[1, 0], [0, 1]]), np.array([1, 2]), rng=rng_mock)
 
-    assert np.array_equal(simple_gen.get(), np.array([[1], [1]]))
+    assert np.array_equal(simple_gen.get(), np.array([[1], [2]]))
     rng_mock.normal.assert_called_with(loc=0, scale=1, size=(2, 1))
 
     rng_mock.normal = Mock(return_value=np.zeros((2, 2)))
-    assert np.array_equal(simple_gen.get(2), np.ones((2, 2)))
+    assert np.array_equal(simple_gen.get(2), [np.ones(2), np.full(2, 2)])
     rng_mock.normal.assert_called_with(loc=0, scale=1, size=(2, 2))
 
     rng_mock.normal = Mock(return_value=np.zeros((2, 100)))
-    assert np.array_equal(simple_gen.get(100), np.ones((2, 100)))
+    assert np.array_equal(simple_gen.get(100), [np.ones(100), np.full(100, 2)])
     rng_mock.normal.assert_called_with(loc=0, scale=1, size=(2, 100))
+
+
+def test_full_shift(rng_mock):
+    rng_mock.normal = Mock(return_value=np.zeros((5, 1)))
+    full_gen = CholeskyGenerator(np.diagflat(np.ones(5)), np.array([0, 1, 2, 3, 4]), rng=rng_mock)
+    assert np.array_equal(full_gen.get(), np.array([[0], [1], [2], [3], [4]]))
+
+    rng_mock.normal = Mock(return_value=np.zeros((5, 100)))
+    assert np.array_equal(full_gen.get(100), np.full((5, 100), [[0], [1], [2], [3], [4]]))
+
+
+def test_distributions():
+    full_gen = CholeskyGenerator(np.diagflat(np.ones(5)), np.array([0, 1, 2, 3, 4]))
+    result = full_gen.get(1000)
+    assert not np.array_equal(result, np.full((5, 100), [[0], [1], [2], [3], [4]]))
+
+    closeness = np.isclose(result.mean(axis=1), [0, 1, 2, 3, 4], atol=0.1)
+    assert closeness.all()
+
+def test_mult(rng_mock):
+    simple_gen = CholeskyGenerator(np.array([[1, 0], [0, 1]]), np.array([0, 0]), np.array([2, -1]), rng=rng_mock)
+
+    rng_mock.normal = Mock(return_value=np.ones((2, 1)))
+    assert np.array_equal(simple_gen.get(), np.array([[2], [-1]]))
+
+    rng_mock.normal = Mock(return_value=np.ones((2, 2)))
+    assert np.array_equal(simple_gen.get(2), [np.full(2, 2), np.full(2, -1)])
+
+    rng_mock.normal = Mock(return_value=np.ones((2, 100)))
+    assert np.array_equal(simple_gen.get(100), [np.full(100, 2), np.full(100, -1)])
