4976995913034f358b67e12fcd76032af5e12a37
Andrew <arharris@mun.ca>
2024-11-20 15:38:06 -0330

    Separate Flask embedding from Client

--- flaskFedAvg-client.py
+++ flaskFedAvg-client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 from flaskFederated.Client import Client
-from linearRegression.utils import get_splitted_dataset, get_code_params
+from linearRegression.utils import get_splitted_dataset, get_code_params, get_weights_dejsonified, get_weights_jsonified
+from flask import Flask, request
 import sys
 from numpy.random import randint
-
+import requests
 
 if(not (len(sys.argv) > 1 and len(sys.argv) < 4)):
     print("Wrong format! Exiting...")
@@ -20,13 +21,30 @@ codeParams = get_code_params(jsonPath)
 numSplits = 5
 data_X, data_Y = get_splitted_dataset(numSplits = numSplits, columnIdx = codeParams["columnIdx"])
 
-client = Client(
-    name = codeParams["name"],
-    ip = codeParams["ip"],
-    server_ip = codeParams["server_ip"]
-)
+client = Client(name = codeParams["name"])
 
 rand_idx = randint(numSplits)
 client.add_data(new_X = data_X[rand_idx], new_Y = data_Y[rand_idx])
 
-client.start_flask()
+app = Flask(__name__)
+
+@app.route("/train", methods=["POST"])
+def train_model():
+    if(not request.json):
+        return None
+
+    weights = get_weights_dejsonified(request.json["new_weights"])
+
+    report = client.train(weights)
+    if not report:
+        return None
+    
+    report["weights"] = get_weights_jsonified(report["weights"])
+    requests.post(f"http://{codeParams['server_ip']}/update_weights", json = report)
+    return report
+
+if __name__ == '__main__':
+    ip, port = codeParams["ip"].split(":")
+    requests.post(f"http://{codeParams['server_ip']}/new_client", json = {"ip": codeParams["ip"]})
+    app.run(host = ip, port = int(port))
+
--- flaskFederated/Client.py
+++ flaskFederated/Client.py
@@ -1,19 +1,13 @@
 import numpy as np
 from linearRegression.models import get_model
-from linearRegression.utils import fit_model, get_loss, get_params, get_weights_dejsonified, get_weights_jsonified
-from flask import Flask, request
-import requests
+from linearRegression.utils import fit_model, get_loss, get_params
 
 class Client():
-    def __init__(self, name, ip, server_ip):
+    def __init__(self, name):
         self.name = name
         self.data_X = []
         self.data_Y = []
         self.model = None
-        self.app = Flask(__name__)
-        self.app.add_url_rule("/train", view_func = self.train, methods = ["POST"])
-        self.ip = ip
-        self.server_ip = server_ip
 
     def add_data(self, new_X, new_Y):
         if(new_X.shape[0] == 1):
@@ -23,15 +17,10 @@ class Client():
             self.data_X.extend(new_X)
             self.data_Y.extend(new_Y)            
 
-    def train(self):
+    def train(self, weights: np.ndarray):
         if(len(self.data_X) == 0 or len(self.data_Y) == 0):
             print("Waiting for data...")
             return None
-        
-        if(not request.json):
-            return None
-
-        weights = get_weights_dejsonified(request.json["new_weights"])
 
         print(f"Start Training {self.name}...")
         report = {}
@@ -50,14 +39,7 @@ class Client():
         print(f"Weight {self.name}: {w}")
         print(f"Bias {self.name}: {b}")
 
-        report["weights"] = get_weights_jsonified(report["weights"])
-        requests.post(f"http://{self.server_ip}/update_weights", json = report)
         return report
 
     def get_model(self):
         return self.model
-    
-    def start_flask(self):
-        ip, port = self.ip.split(":")
-        requests.post(f"http://{self.server_ip}/new_client", json = {"ip": self.ip})
-        self.app.run(host = ip, port = int(port))
